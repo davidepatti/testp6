dea4b3ddc1e4bb4d85f73f7dd62a0d1f0a611ece
davidepatti <xedivad@gmail.com>
2022-05-23 16:29:30 +0200

    aggiunto saluto

--- Main.java
+++ Main.java
@@ -3,5 +3,20 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
+        System.out.println("Ciao mondo!");
     }
 }
